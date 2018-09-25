549adb111424289c978fb966578014c2c7c79034
Tyler <1251424057@qq.com>
2018-09-25 22:17:59 +0800

    asd

---
+++ 559.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
+#include <numeric>
+
+using namespace std;
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution
+{
+  public:
+    int maxDepth(Node *root)
+    {
+        if (!root)
+            return 0;
+        int ans = 0;
+        for (auto i : root->children)
+            ans = max(ans, maxDepth(i));
+        return ans + 1;
+    }
+};
