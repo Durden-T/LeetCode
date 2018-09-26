6fd7ef30b6377371d60e1bd968991d91b7ec53bd
Tyler <1251424057@qq.com>
2018-09-26 21:10:23 +0800

    asd

---
+++ 606.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
+#include <numeric>
+#include <cstring>
+
+using namespace std;
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    string tree2str(TreeNode *t)
+    {
+        if (!t)
+            return "";
+        if (!t->left && !t->right)
+            return to_string(t->val);
+        if (!t->right)
+            return to_string(t->val) + "(" + tree2str(t->left) + ")";
+        return to_string(t->val) + "(" + tree2str(t->left) + ")(" + tree2str(t->right) + ")";
+    }
+};
