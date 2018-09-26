19b537b0711caebc5ba9c8b1ab5ff796918d3b30
Tyler <1251424057@qq.com>
2018-09-26 20:04:11 +0800

    asd

---
+++ 598.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
+#include <numeric>
+#include <cstring>
+
+using namespace std;
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    int maxCount(int m, int n, vector<vector<int>> &ops)
+    {
+        for (auto &p : ops)
+        {
+            m = min(m, p[0]);
+            n = min(n, p[1]);
+        }
+        return m * n;
+    }
+};
