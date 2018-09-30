04312227dda4fc0d62f4f62927b79db26d0aaddb
Tyler <1251424057@qq.com>
2018-09-30 14:47:45 +0800

    asd

--- 696.cpp
+++ 696.cpp
@@ -23,3 +23,53 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int countBinarySubstrings(string s)
+    {
+        int choose = 1;
+        if (choose == 0)
+        {
+            int ans = 0, count0 = 0, count1 = 0;
+            if (s[0] == '0')
+                ++count0;
+            else
+                ++count1;
+            for (int i = 1; i < s.size(); ++i)
+                if (s[i] == '0')
+                {
+                    ++count0;
+                    if (s[i - 1] == '1')
+                        count0 = 1;
+                    if (count0 <= count1)
+                        ++ans;
+                }
+                else if (s[i] == '1')
+                {
+                    ++count1;
+                    if (s[i - 1] == '0')
+                        count1 = 1;
+                    if (count1 <= count0)
+                        ++ans;
+                }
+            return ans;
+        }
+        else if (choose == 1)
+        {
+            int ans = 0, start = 0, end = 0, pre = 0, cur = 0;
+            while (start < s.size())
+            {
+                while (end + 1 < s.size() && s[end + 1] == s[start])
+                    ++end;
+                int cur = end - start + 1;
+                ans += min(cur, pre);
+                pre = cur;
+                start = end + 1;
+                end = start;
+            }
+            return ans;
+        }
+    }
+};
