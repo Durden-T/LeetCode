dc480e170f1dde4cfda652e8bad4e761cf3e6531
Tyler <1251424057@qq.com>
2018-09-30 12:48:26 +0800

    asd

--- 682.cpp
+++ 682.cpp
@@ -23,3 +23,24 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int calPoints(vector<string> &ops)
+    {
+        vector<int> rlt;
+        for (const string &o : ops)
+        {
+            if (o == "C")
+                rlt.pop_back();
+            else if (o == "D")
+                rlt.push_back(rlt.back() * 2);
+            else if (o == "+")
+                rlt.push_back(rlt.back() + rlt[rlt.size() - 2]);
+            else
+                rlt.push_back(stoi(o));
+        }
+        return accumulate(rlt.begin(), rlt.end(), 0);
+    }
+};
