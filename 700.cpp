e870e6776b6b5e551cf9dc43d4f38576e9e309ef
Tyler <1251424057@qq.com>
2018-09-30 15:59:17 +0800

    asd

---
+++ 700.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
+#include <numeric>
+
+using namespace std;
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (!root || root->val == val)
+            return root;
+        return root->val < val ? searchBST(root->right, val) : searchBST(root->left, val);
+    }
+};
