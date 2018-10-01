50a57245523f5d25d751b2c9babf02cb6cf38325
Tyler <1251424057@qq.com>
2018-10-01 15:21:44 +0800

    as

--- 836.cpp
+++ 836.cpp
@@ -24,3 +24,24 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    bool isRectangleOverlap(vector<int> &rec1, vector<int> &rec2)
+    {
+        int choose = 1;
+        if(choose==0)
+        {
+            return !(rec1[2] <= rec2[0] || // left
+                     rec1[3] <= rec2[1] || // bottom
+                     rec1[0] >= rec2[2] || // right
+                     rec1[1] >= rec2[3]);  // top
+        }
+        else if(choose==1)
+        {
+            return (min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]) && // width > 0
+                    min(rec1[3], rec2[3]) > max(rec1[1], rec2[1]));  // height > 0
+        }
+    }
+};
