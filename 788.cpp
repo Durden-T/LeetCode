293091c31c47aa2649ab785ba56b2d3c36dfa405
Tyler <1251424057@qq.com>
2018-10-01 09:32:56 +0800

    asd

--- 788.cpp
+++ 788.cpp
@@ -24,3 +24,52 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int rotatedDigits(int N)
+    {
+        int ans = 0;
+        for (int i = 1; i <= N; ++i)
+        {
+            int t = i, _new = 0, limit = 1;
+            bool valid = true;
+            while (t /= 10)
+                limit *= 10;
+            t = i;
+            while (valid && limit)
+            {
+                int cur = t / limit;
+                switch (cur)
+                {
+                case 0:
+                case 1:
+                case 8:
+                    break;
+                case 2:
+                    cur = 5;
+                    break;
+                case 5:
+                    cur = 2;
+                    break;
+                case 6:
+                    cur = 9;
+                    break;
+                case 9:
+                    cur = 6;
+                    break;
+                default:
+                    valid = false;
+                    break;
+                }
+                _new = 10 * _new + cur;
+                t %= limit;
+                limit /= 10;
+            }
+            if (valid && _new != i)
+                ++ans;
+        }
+        return ans;
+    }
+};
