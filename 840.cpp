d9e6666e83519ad3524cc6a2af1160f53e4f0e1e
Tyler <1251424057@qq.com>
2018-10-01 15:45:03 +0800

    asd

--- 840.cpp
+++ 840.cpp
@@ -24,3 +24,38 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int numMagicSquaresInside(vector<vector<int>> &grid)
+    {
+        if (grid.size() < 3 || grid[0].size() < 3)
+            return 0;
+        int ans = 0;
+        for (int i = 0; i < grid.size() - 2; ++i)
+            for (int j = 0; j < grid[0].size() - 2; ++j)
+                ans += grid[i + 1][j + 1] == 5 && helper(grid, i, j);
+        return ans;
+    }
+
+  private:
+    bool helper(vector<vector<int>> &grid, int r, int c)
+    {
+        if (grid[r][c] == 5) //check there are duplicates or not
+            return false;
+        for (int i = r; i < r + 3; ++i)
+            for (int j = c; j < c + 3; ++j)
+                if (grid[i][j] > 9)
+                    return false;
+
+        return grid[r][c] + grid[r][c + 1] + grid[r][c + 2] == 15 &&
+               grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2] == 15 &&
+               grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2] == 15 &&
+               grid[r][c] + grid[r + 1][c] + grid[r + 2][c] == 15 &&
+               grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1] == 15 &&
+               grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2] == 15 &&
+               grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] == 15 &&
+               grid[r + 2][c] + grid[r + 1][c + 1] + grid[r][c + 2] == 15;
+    }
+};
