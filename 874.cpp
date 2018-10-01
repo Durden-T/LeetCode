b22c76b9fcfe999978b91b6169b3314a97d0d72b
Tyler <1251424057@qq.com>
2018-10-01 21:47:01 +0800

    asd

--- 874.cpp
+++ 874.cpp
@@ -28,14 +28,38 @@ static int __initialSetup = [] {
 class Solution
 {
   public:
-    vector<vector<int>> flipAndInvertImage(vector<vector<int>> &A)
+    int robotSim(vector<int> &commands, vector<vector<int>> &obstacles)
     {
-        for (auto &row : A)
+        int dx[4] = {0, 1, 0, -1};
+        int dy[4] = {1, 0, -1, 0};
+        int x = 0, y = 0, di = 0;
+
+        set<pair<int, int>> obstacleSet;
+        for (vector<int> &obstacle : obstacles)
+            obstacleSet.emplace(obstacle[0], obstacle[1]);
+
+        int ans = 0;
+        for (int cmd : commands)
         {
-            reverse(row.begin(), row.end());
-            for (int &i : row)
-                i ^= 1;
+            if (cmd == -2)
+                di = (di + 3) % 4;
+            else if (cmd == -1)
+                di = (di + 1) % 4;
+            else
+                for (int k = 0; k < cmd; ++k)
+                {
+                    int nx = x + dx[di];
+                    int ny = y + dy[di];
+                    if (obstacleSet.find({nx, ny}) == obstacleSet.end())
+                    {
+                        x = nx;
+                        y = ny;
+                        ans = max(ans, x * x + y * y);
+                    }
+                    else
+                        break;
+                }
         }
-        return A;
+        return ans;
     }
 };
