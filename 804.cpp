6b3ea0d75239e89b3d919936fd48a06eb92f1adc
Tyler <1251424057@qq.com>
2018-10-01 10:02:45 +0800

    asd

---
+++ 804.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
+#include <numeric>
+#include <bitset>
+
+using namespace std;
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    int uniqueMorseRepresentations(vector<string> &words)
+    {
+        string table[26] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        unordered_set<string> S;
+        for(string &word:words)
+        {
+            string _new;
+            for(char c:word)
+                _new += table[c - '26'];
+            S.insert(_new);
+        }
+        return S.size();
+    }
+};
