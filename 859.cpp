6bef7ff8a0264ca42e7daa58b5082220c34fa04c
Tyler <1251424057@qq.com>
2018-10-01 19:31:38 +0800

    asd

--- 859.cpp
+++ 859.cpp
@@ -24,3 +24,44 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    bool buddyStrings(string A, string B)
+    {
+        if (A.size() != B.size())
+            return false;
+        if (A == B)
+        {
+            int map[26] = {0};
+            for (int i : A)
+                if (++map[i - 'a'] > 1)
+                    return true;
+            return false;
+        }
+        int first = -1, i = 0;
+        while (i < A.size())
+        {
+            if (A[i] != B[i])
+            {
+                if (first == -1)
+                    first = i;
+                else
+                {
+                    swap(A[i], A[first]);
+                    if (A[i] != B[i] || A[first] != B[first])
+                        return false;
+                    else
+                        break;
+                }
+            }
+            ++i;
+        }
+        if (i == A.size())
+            return false;
+        while (i < A.size() && A[i] == B[i])
+            ++i;
+        return i == A.size();
+    }
+};
