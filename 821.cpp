4b751de0729d4b6454a5d026cfa0e8bd9172d6a9
Tyler <1251424057@qq.com>
2018-10-01 12:40:51 +0800

    asd

--- 821.cpp
+++ 821.cpp
@@ -30,5 +30,39 @@ class Solution
   public:
     vector<int> shortestToChar(string S, char C)
     {
+        vector<int> ans(S.size(), S.size());
+        int pos = -S.size();
+        for (int i = 0; i < S.size(); ++i)
+        {
+            if (S[i] == C)
+                pos = i;
+            ans[i] = i - pos;
+        }
+        for (int i = S.size() - 1; i >= 0; --i)
+        {
+            if (S[i] == C)
+                pos = i;
+            ans[i] = min(ans[i], abs(i - pos));
+        }
+        return ans;
+
+
+        vector<int> ans(S.size(), INT_MAX);
+        int i = 0;
+
+        for (int i = 0; i < S.size(); ++i)
+            if (S[i] == C)
+            {
+                ans[i] = 0;
+                for (int j = i - 1; j >= 0 && S[j] != C; --j)
+                    ans[j] = i - j;
+            }
+
+        for (int i = S.size() - 1; i >= 0; --i)
+            if (S[i] == C)
+                for (int j = i + 1; j < S.size() && S[j] != C; ++j)
+                    ans[j] = min(ans[j], j - i);
+
+        return ans;
     }
 };
