809a3476358f9cb967a26f018d83962e6a9fa1ce
Tyler <1251424057@qq.com>
2018-10-02 10:38:11 +0800

    asd

--- 905.cpp
+++ 905.cpp
@@ -30,5 +30,9 @@ class Solution
   public:
     vector<int> sortArrayByParity(vector<int> &A)
     {
+        partition(A.begin(), A.end(), [](int i) {
+            return !(i & 1);
+        });
+        return A;
     }
 };
