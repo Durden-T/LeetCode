9d2410409df120bc5fbc6f45518e25e723b83fa0
Tyler <1251424057@qq.com>
2018-10-03 20:58:19 +0800

    asd

--- 12.cpp
+++ 12.cpp
@@ -25,3 +25,32 @@ static int __initialSetup = [] {
     return 0;
 }();
 
+class Solution
+{
+  public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        int diff = INT_MAX, ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; ++i)
+        {
+            int j = i + 1, k = nums.size() - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (abs(sum - target) < diff)
+                {
+                    diff = abs(sum - target);
+                    ans = sum;
+                }
+                if (sum < target)
+                    ++j;
+                else if (sum > target)
+                    --k;
+                else
+                    return target;
+            }
+        }
+        return ans;
+    }
+};
