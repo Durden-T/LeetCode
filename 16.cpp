4951f96c78623e4146297d88713b0d26d25e7cef
Tyler <1251424057@qq.com>
2018-10-03 21:25:05 +0800

    asd

--- 16.cpp
+++ 16.cpp
@@ -25,3 +25,30 @@ static int __initialSetup = [] {
     return 0;
 }();
 
+class Solution
+{
+  public:
+    int divide(int dividend, int divisor)
+    {
+        bool negative = (dividend < 0) ^ (divisor < 0);
+        long long dvd = llabs(dividend);
+        long long dvs = llabs(divisor);
+        long long ans = 0;
+        while (dvd >= dvs)
+        {
+            long long temp = dvs, multiple = 1;
+            while (dvd >= (temp << 1))
+            {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            ans += multiple;
+        }
+        if (negative)
+            ans = -ans;
+        if (ans > INT_MAX || ans < INT_MIN)
+            return INT_MAX;
+        return ans;
+    }
+};
