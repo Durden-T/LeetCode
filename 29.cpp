95264615dee735090f68c4c4caa8ea41c0842a01
Tyler <1251424057@qq.com>
2018-10-03 21:52:30 +0800

    ads

--- 29.cpp
+++ 29.cpp
@@ -25,3 +25,15 @@ static int __initialSetup = [] {
     return 0;
 }();
 
+class Solution
+{
+  public:
+    void nextPermutation(vector<int> &nums)
+    {
+        auto it = is_sorted_until(nums.rbegin(), nums.rend());
+        if (it != nums.rend())
+            swap(*it, *upper_bound(nums.rbegin(), it, *it));
+        reverse(nums.rbegin(), it);
+    }
+};
+
