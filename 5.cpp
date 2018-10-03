49f8f1ff01cfe4080ab24ccb8486484365d3394c
Tyler <1251424057@qq.com>
2018-10-03 19:15:32 +0800

    asd

--- 5.cpp
+++ 5.cpp
@@ -25,3 +25,57 @@ static int __initialSetup = [] {
     return 0;
 }();
 
+class Solution
+{
+  public:
+    string longestPalindrome(string s)
+    {
+        int choose = 1;
+        if (choose == 0)
+        {
+            if (s.size() < 2)
+                return s;
+            bool dp[s.size()][s.size()] = {};
+            int max = 1, start = 0;
+            for (int i = 0; i < s.size(); ++i)
+                for (int j = 0; j <= i; ++j)
+                    if ((dp[j][i] = s[i] == s[j] && (i - j < 2 || dp[j + 1][i - 1])) && max < i - j + 1)
+                    {
+                        max = i - j + 1;
+                        start = j;
+                    }
+            return s.substr(start, max);
+        }
+
+        else if (choose == 1)
+        {
+            if (s.size() < 2)
+                return s;
+            int start = 0, ans = 0;
+            for (int i = 0; i < s.size(); ++i)
+            {
+                int len1 = expandAroundCenter(s, i, i);
+                int len2 = expandAroundCenter(s, i, i + 1);
+                int len = max(len1, len2);
+                if (len > ans)
+                {
+                    start = i - (len - 1) / 2;
+                    ans = len;
+                }
+            }
+            return s.substr(start, ans);
+        }
+    }
+
+  private:
+    int expandAroundCenter(string &s, int left, int right)
+    {
+        int L = left, R = right;
+        while (L >= 0 && R < s.size() && s[L] == s[R])
+        {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+};
