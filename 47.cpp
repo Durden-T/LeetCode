1d0c2c55243cda621660ac8ec59432b9aef0ac9d
Tyler <1251424057@qq.com>
2018-10-04 10:31:40 +0800

    asd

--- 47.cpp
+++ 47.cpp
@@ -25,3 +25,75 @@ static int __initialSetup = [] {
     return 0;
 }();
 
+class Solution
+{
+    vector<vector<int>> ans;
+    unordered_set<int> s;
+
+  public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        if (nums.empty())
+            return ans;
+        helper(nums, 0);
+        return ans;
+    }
+
+  private:
+    void helper(vector<int> &nums, int pos)
+    {
+        if (pos == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = pos; i < nums.size(); ++i)
+            if (!s.count(i))
+            {
+                s.insert(i);
+                swap(nums[pos], nums[i]);
+                helper(nums, pos + 1);
+                swap(nums[pos], nums[i]);
+            }
+    }
+};
+
+static int __initialSetup = [] {
+    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(nullptr);            // untie cin & cout
+    return 0;
+}();
+
+class Solution
+{
+    vector<vector<int>> ans;
+
+  public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        if (nums.empty())
+            return ans;
+        helper(nums, 0);
+        return ans;
+    }
+
+  private:
+    void helper(vector<int> &nums, int pos)
+    {
+        if (pos == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = pos; i < nums.size(); ++i)
+        {
+            if(i!=pos&&nums[i]==nums[pos])
+                continue;
+            swap(nums[pos], nums[i]);
+            helper(nums, pos + 1);
+            swap(nums[pos], nums[i]);
+        }
+    }
+};
