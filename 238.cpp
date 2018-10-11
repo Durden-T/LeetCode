169a38a05441d6e902ae83197b531ceea2023875
Tyler <1251424057@qq.com>
2018-10-11 13:47:18 +0800

    asd

--- 238.cpp
+++ 238.cpp
@@ -30,16 +30,34 @@ class Solution
   public:
     vector<int> productExceptSelf(vector<int> &nums)
     {
-        int n = nums.size();
-        vector<int> fromBegin(n), fromLast(n), res(n);
-        fromBegin[0] = fromLast[0] = 1;
-        for (int i = 1; i < n; ++i)
+        int choose = 1;
+        if (choose == 0)
         {
-            fromBegin[i] = fromBegin[i - 1] * nums[i - 1];
-            fromLast[i] = fromLast[i - 1] * nums[n - i];
+            int n = nums.size();
+            vector<int> fromBegin(n), fromLast(n), res(n);
+            fromBegin[0] = fromLast[0] = 1;
+            for (int i = 1; i < n; ++i)
+            {
+                fromBegin[i] = fromBegin[i - 1] * nums[i - 1];
+                fromLast[i] = fromLast[i - 1] * nums[n - i];
+            }
+            for (int i = 0; i < n; ++i)
+                res[i] = fromBegin[i] * fromLast[n - 1 - i];
+            return res;
         }
-        for (int i = 0; i < n; ++i)
-            res[i] = fromBegin[i] * fromLast[n - 1 - i];
-        return res;
+
+        else if (choose == 1)
+        {
+            int n = nums.size(), fromBegin = 1, fromLast = 1;
+            vector<int> res(n, 1);
+            for (int i = 0; i < n; ++i)
+            {
+                res[i] *= fromBegin;
+                fromBegin *= nums[i];
+                res[n - 1 - i] *= fromLast;
+                fromLast *= nums[n - 1 - i];
+            }
+            return res;
+        }
     }
 };
