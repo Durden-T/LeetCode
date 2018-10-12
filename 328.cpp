2a745764b3044e2417ee4874e4e1538a179c60e2
Tyler <1251424057@qq.com>
2018-10-12 18:54:13 +0800

    asd

--- 328.cpp
+++ 328.cpp
@@ -24,3 +24,24 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (!head)
+            return nullptr;
+        ListNode * odd = head,*even = head->next,*evenHead = even;
+        while (even && even->next)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
