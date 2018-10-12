4ac83888297e16c959ae81f06dd30ab35a2bcf27
Tyler <1251424057@qq.com>
2018-10-12 21:42:53 +0800

    asd

--- 332.cpp
+++ 332.cpp
@@ -24,3 +24,35 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+    multimap<string, string> m;
+    vector<string> ans;
+    multimap<string, string>::iterator it;
+
+  public:
+    vector<string> findItinerary(vector<pair<string, string>> tickets)
+    {
+        sort(tickets.begin(), tickets.end());
+        for (auto &p : tickets)
+            m.insert(p);
+        vector<string> t = {"JFK"};
+        helper(t, tickets.size() + 1);
+        return ans;
+    }
+    void helper(vector<string> &t, int size)
+    {
+        while (ans.empty() && (it = m.find(t.back())) != m.end())
+        {
+            t.push_back(it->second);
+            m.erase(it);
+            auto p = *it;
+            helper(t,size);
+            t.pop_back();
+            m.insert(p);
+        }
+        if (t.size() == size)
+            ans = t;
+    }
+};
