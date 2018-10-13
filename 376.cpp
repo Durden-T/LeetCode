f96d5e798937020b3e6b4696265857533ffc7fd0
Tyler <1251424057@qq.com>
2018-10-13 23:57:37 +0800

    asd

--- 376.cpp
+++ 376.cpp
@@ -24,3 +24,44 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int wiggleMaxLength(vector<int> &nums)
+    {
+        int choose = 1;
+        if (choose == 0)
+        {
+            if (nums.size() < 2)
+                return nums.size();
+            int prediff = nums[1] - nums[0], ans = (prediff != 0) + 1;
+            for (int i = 2; i < nums.size(); ++i)
+            {
+                int diff = nums[i] - nums[i - 1];
+                if (diff > 0 && prediff <= 0 || diff < 0 && prediff >= 0)
+                {
+                    ++ans;
+                    prediff = diff;
+                }
+            }
+            return ans;
+        }
+
+        else if (choose == 1) //https://leetcode.com/problems/wiggle-subsequence/solution/
+        {
+            if (nums.size() < 2)
+                return nums.size();
+            int up = 1, down = 1;
+            for (int i = 1; i < nums.size(); i++)
+            {
+
+                if (nums[i] > nums[i - 1])
+                    up = down + 1;
+                else if (nums[i] < nums[i - 1])
+                    down = up + 1;
+            }
+            return max(up, down);
+        }
+    }
+};
