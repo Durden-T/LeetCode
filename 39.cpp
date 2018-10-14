36e07641d659e80ab5907923da1d1c29f1f33a6a
Tyler <1251424057@qq.com>
2018-10-14 20:42:54 +0800

    asd

--- 39.cpp
+++ 39.cpp
@@ -24,3 +24,43 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+    vector<vector<int>> ans;
+    vector<int> path;
+
+  public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        if (candidates.empty())
+            return {};
+        if (candidates.size() == 1)
+        {
+            if (!(target % candidates[0]))
+                ans.emplace_back(target / candidates[0], candidates[0]);
+            return ans;
+        }
+        sort(candidates.begin(), candidates.end());
+        if (candidates[0] > target)
+            return ans;
+        helper(candidates, target, 0);
+        return ans;
+    }
+
+  private:
+    void helper(vector<int> &a, int target, int start)
+    {
+        if (!target)
+        {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = start; i < a.size() && a[i] <= target; ++i)
+        {
+            path.push_back(a[i]);
+            helper(a, target - a[i], i);
+            path.pop_back();
+        }
+    }
+};
