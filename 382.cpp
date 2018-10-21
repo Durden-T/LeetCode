bdaaeebfa9a00abecd56ecf7fc4025ee7514aa66
Tyler <1251424057@qq.com>
2018-10-21 19:16:08 +0800

    asd

--- 382.cpp
+++ 382.cpp
@@ -24,3 +24,33 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+
+class Solution
+{
+  private:
+    ListNode *head;
+
+  public:
+    /** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+    Solution(ListNode *head)
+    {
+        this->head = head;
+    }
+
+    /** Returns a random node's value. */
+    int getRandom()
+    {
+        int ans = head->val;
+        ListNode *node = head->next;
+        int i = 2;
+        while (node)
+        {
+            int j = rand() % i++;
+            if (!j)
+                ans = node->val;
+            node = node->next;
+        }
+        return ans;
+    }
+};
