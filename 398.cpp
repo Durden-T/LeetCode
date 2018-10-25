b5e7cb8f0413c2ec82e995e84239214b8f48dff3
Tyler <1251424057@qq.com>
2018-10-25 14:59:38 +0800

    asd

--- 398.cpp
+++ 398.cpp
@@ -24,3 +24,41 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    vector<int> A;
+    Solution(vector<int> v) : A(v) {}
+
+    int pick(int Target)
+    {
+        int ans;
+        for (int i = 0, cnt = 0; i < A.size(); ++i)
+            if (A[i] == Target && !(rand() % ++cnt))
+                ans = i;
+        return ans;
+    }
+};
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
+/*class Solution
+{
+    unordered_map<int, vector<int>> mp;
+
+  public:
+    Solution(vector<int> nums)
+    {
+        for (int i = 0; i < nums.size(); ++i)
+            mp[nums[i]].push_back(i);
+    }
+
+    int pick(int target)
+    {
+        auto &v = mp[target];
+        return v[rand() % v.size()];
+    }
+};*/
