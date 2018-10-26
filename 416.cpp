9b7e74e6c3e7247cd77cd4778e2382b0946e606c
Tyler <1251424057@qq.com>
2018-10-26 15:59:38 +0800

    asd

--- 416.cpp
+++ 416.cpp
@@ -24,3 +24,54 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    bool canPartition(vector<int> &nums)
+    {
+        int choose = 2;
+        if (choose == 0)
+        {
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            if (sum & 1)
+                return false;
+            sort(nums.begin(), nums.end(), greater<int>());
+            return helper(nums, 0, sum >> 1);
+        }
+        else if (choose == 1)
+        {
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            if (sum & 1)
+                return false;
+            int target = sum >> 1;
+            bool dp[target + 1] = {true};
+            for (int num : nums)
+            {
+                if (dp[target])
+                    return true;
+                for (int i = target; i >= num && !dp[i]; --i)
+                    dp[i] |= dp[i - num];
+            }
+            return dp[target];
+        }
+        else if (choose == 2) //???
+        {
+            bitset<5001> bits(1);
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            for (auto n : nums)
+                bits |= bits << n;
+            return !(sum & 1) && bits[sum >> 1];
+        }
+    }
+
+  private:
+    bool helper(vector<int> &nums, int i, int target)
+    {
+        if (target == nums[i])
+            return true;
+        if (target < nums[i])
+            return false;
+        return helper(nums, i + 1, target - nums[i]) || helper(nums, i + 1, target);
+    }
+};
--- 417.cpp
+++ 417.cpp
@@ -24,3 +24,66 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+/*
+        mark[m][n] <- 0 : 00 <- untouched  01<- pacific only 10 <- atlantic only 11 <- both. 
+        for i <- 0..m and j <- 0:
+            helper() upwards and visit new nodes
+        for i <- 0 and j <- 0..n:
+            helper() upwards and visit new nodes
+        do the same for right edge and bottom edge and mark atlantic.
+        
+        iterate all nodes and list the nodes that can be reached by both.
+            
+    */
+class Solution
+{
+    vector<pair<int, int>> ans;
+    int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    enum
+    {
+        Pacific = 1,
+        Atlantic,
+        both
+    };
+
+  public:
+    vector<pair<int, int>> pacificAtlantic(vector<vector<int>> &matrix)
+    {
+        if (matrix.empty() || matrix[0].empty())
+            return {};
+
+        vector<vector<int>> table(matrix.size(), vector<int>(matrix[0].size(), 0));
+
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            helper(matrix, table, i, 0, Pacific);
+            helper(matrix, table, i, matrix[0].size() - 1, Atlantic);
+        }
+
+        for (int i = 0; i < matrix[0].size(); ++i)
+        {
+            helper(matrix, table, 0, i, Pacific);
+            helper(matrix, table, matrix.size() - 1, i, Atlantic);
+        }
+
+        return ans;
+    }
+
+  private:
+    void helper(vector<vector<int>> &matrix, vector<vector<int>> &table, int i, int j, int ocean)
+    {
+        if (i < 0 || i == matrix.size() || j < 0 || j == matrix[0].size() || table[i][j] == ocean)
+            return;
+
+        if (table[i][j] != both && (table[i][j] |= ocean) == both)
+            ans.emplace_back(i, j);
+
+        for (int k = 0; k < 4; ++k)
+        {
+            int ni = i + dir[k][0], nj = j + dir[k][1];
+            if (ni >= 0 && ni < matrix.size() && nj >= 0 && nj < matrix[0].size() && matrix[i][j] <= matrix[ni][nj])
+                helper(matrix, table, ni, nj, table[i][j]);
+        }
+    }
+};
