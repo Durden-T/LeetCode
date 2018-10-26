30a751168e4a7af22147007b81b91c1fce40f5cb
Tyler <1251424057@qq.com>
2018-10-26 16:32:16 +0800

    asd

--- 419.cpp
+++ 419.cpp
@@ -24,3 +24,21 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int countBattleships(vector<vector<char>> &board)
+    {
+        if (board.empty() || board[0].empty())
+            return 0;
+            
+        int ans = 0;
+
+        for (int i = 0; i < board.size(); ++i)
+            for (int j = 0; j < board[0].size(); ++j)
+                ans += board[i][j] == 'X' && (!i || board[i - 1][j] != 'X') && (!j || board[i][j - 1] != 'X');
+
+        return ans;
+    }
+};
