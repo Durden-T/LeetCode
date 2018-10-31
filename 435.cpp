6b606b59602c3295ed9ab7c5d99b153acf217b0d
Tyler <1251424057@qq.com>
2018-10-31 18:38:47 +0800

    asd

--- 435.cpp
+++ 435.cpp
@@ -24,3 +24,39 @@ static int __initialSetup = [] {
     cin.tie(nullptr);            // untie cin & cout
     return 0;
 }();
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution
+{
+  public:
+    int eraseOverlapIntervals(vector<Interval> &intervals)
+    {
+        if (intervals.size() < 2)
+            return 0;
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+            return a.start < b.start;
+        });
+        int ans = 0;
+        auto last = &intervals.front();
+        for (int i = 1; i < intervals.size(); ++i)
+        {
+            if (intervals[i].start != last->start && intervals[i].start >= last->end)
+                last = &intervals[i];
+            else
+            {
+                ++ans;
+                if (last->end > intervals[i].end)
+                    last = &intervals[i];
+            }
+        }
+        return ans;
+    }
+};
