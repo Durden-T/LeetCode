d89995e080350310c939cc7c9c4051fc7ae8e281
Tyler <1251424057@qq.com>
2018-11-06 23:03:42 +0800

    asd

--- 451.cpp
+++ 451.cpp
@@ -20,7 +20,37 @@
 using namespace std;
 
 static int __initialSetup = [] {
-    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
-    cin.tie(nullptr);            // untie cin & cout
+    ios::sync_with_stdio(0); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(0);              // untie cin & cout
+    cout.tie(0);
     return 0;
 }();
+
+class Solution
+{
+    struct
+    {
+        bool operator()(const pair<char, int> &a, const pair<char, int> &b)
+        {
+            return a.second > b.second;
+        };
+    } cmp;
+
+  public:
+    string frequencySort(string s)
+    {
+        vector<pair<char, int>> count(256);
+        for (int i = 0; i < 256; ++i)
+            count[i] = {i, 0};
+        for (int c : s)
+            ++count[c].second;
+        sort(count.begin(), count.end(), cmp);
+        for (int i = 0, j = 0; i < 256 && count[i].second; ++i)
+            while (count[i].second)
+            {
+                --count[i].second;
+                s[j++] = count[i].first;
+            }
+        return s;
+    }
+};
--- 452.cpp
+++ 452.cpp
@@ -20,7 +20,30 @@
 using namespace std;
 
 static int __initialSetup = [] {
-    ios::sync_with_stdio(false); // toggle off cout & cin, instead, use printf & scanf
-    cin.tie(nullptr);            // untie cin & cout
+    ios::sync_with_stdio(0); // toggle off cout & cin, instead, use printf & scanf
+    cin.tie(0);              // untie cin & cout
+    cout.tie(0);
     return 0;
 }();
+
+class Solution
+{
+  public:
+    int findMinArrowShots(vector<pair<int, int>> &points)
+    {
+        if (points.size() < 2)
+            return points.size();
+        sort(points.begin(), points.end());
+        int ans = 1, cur = points[0].second;
+        for (int i = 1; i < points.size(); ++i)
+        {
+            if (points[i].first > cur)
+            {
+                ++ans;
+                cur = points[i].second;
+            }
+            cur = min(points[i].second, cur);
+        }
+        return ans;
+    }
+};
